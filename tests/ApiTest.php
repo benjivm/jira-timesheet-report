b03809f01b1e5337af1d34d995ed0c2da4dfaa9a
Ben Miller <benm@kvalinc.com>
2019-04-08 11:33:05 -0700

    Update tests

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -5,11 +5,22 @@ namespace Tests;
 class ApiTest extends BaseTestCase
 {
     /**
-     * Test that the issues API route returns a rendered response.
+     * Test that the issues API route returns a JSON response.
      */
     public function testApiIssuesEndpointLoadsDataFromJira()
     {
         $response = $this->runApp('GET', '/api/issues');
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('application/json;charset=utf-8', $response->getHeader('content-type')[0]);
+    }
+
+    /**
+     * Test that the issues API route returns a JSON response with our query.
+     */
+    public function testApiIssuesEndpointLoadsDataFromJiraWithQuery()
+    {
+        $response = $this->runApp('GET', '/api/issues?createdAfter=2019-04-01&createdBefore=2019-04-13&status=In+Progress&maxResults=50');
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals('application/json;charset=utf-8', $response->getHeader('content-type')[0]);
--- tests/MainPageTest.php
+++ tests/MainPageTest.php
@@ -31,12 +31,13 @@ class MainPageTest extends BaseTestCase
      */
     public function testGetMainPageWithQuery()
     {
-        $response = $this->runApp('GET', '/?createdAfter=2019-04-01&createdBefore=2019-04-13&assignee=&maxResults=250');
-        
+        $response = $this->runApp('GET', '/?createdAfter=2019-04-01&createdBefore=2019-04-13&status=In+Progress&maxResults=50');
+
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertStringContainsString($this->project . ' Timesheet', (string) $response->getBody());
         $this->assertStringContainsString('<input type="date" class="form-control" id="createdAfter" name="createdAfter" value="2019-04-01">', (string) $response->getBody());
         $this->assertStringContainsString('<input type="date" class="form-control" id="createdBefore" name="createdBefore" value="2019-04-13">', (string) $response->getBody());
-        $this->assertStringContainsString('<option value="250" selected>250</option>', (string) $response->getBody());
+        $this->assertStringContainsString('<option value="In Progress" selected>In Progress</option>', (string) $response->getBody());
+        $this->assertStringContainsString('<option value="50" selected>50</option>', (string) $response->getBody());
     }
 }
